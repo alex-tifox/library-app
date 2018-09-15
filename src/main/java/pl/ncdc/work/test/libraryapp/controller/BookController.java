22e0cdf6cd48da935a676f51dd8e0c8de2a06264
foxmr <tilnyi.olexander@gmail.com>
2018-09-15 16:44:59 +0200

    adding books + pages rewritten

--- src/main/java/pl/ncdc/work/test/libraryapp/controller/BookController.java
+++ src/main/java/pl/ncdc/work/test/libraryapp/controller/BookController.java
@@ -5,8 +5,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.stereotype.Service;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import pl.ncdc.work.test.libraryapp.model.Book;
+import pl.ncdc.work.test.libraryapp.model.BookForm;
 import pl.ncdc.work.test.libraryapp.repository.BookRepo;
 
 import java.util.ArrayList;
@@ -34,5 +36,25 @@ public class BookController {
             return "listPage";
         }
     }
+
+    @GetMapping("/addPage")
+    public String showAddBookPage(Model model) {
+        BookForm bookForm = new BookForm();
+        model.addAttribute("bookForm", bookForm);
+
+        return "addPage";
+    }
+
+    @PostMapping("/addPage")
+    public String saveBook(Model model,
+                           @ModelAttribute("bookForm") BookForm bookForm) {
+
+        Book book = new Book(bookForm.getTitle(), bookForm.getAuthor(), bookForm.getIsbn());
+        this.bookRepo.save(book);
+
+        return "redirect:/listPage";
+    }
+
+
 
 }
